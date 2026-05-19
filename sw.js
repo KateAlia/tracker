77859998ea6a08f00eb6f3210b2476087e3b91fe
FosseGrely <fossegrely@gmail.com>
2026-05-19 10:01:48 +0200

    first commit

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'tracker-v1';
+const FILES = [
+  './tracker_12week.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
